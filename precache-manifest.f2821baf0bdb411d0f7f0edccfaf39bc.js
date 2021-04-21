e37f693bda2bd257bbc1029f04e266fbfe4648c5
unknown <anshuman1307@yahoo.co.in>
2021-04-22 03:08:01 +0530

    Updates

---
+++ precache-manifest.f2821baf0bdb411d0f7f0edccfaf39bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a64f8557d93867bd6a781d712a562b",
+    "url": "/recro/index.html"
+  },
+  {
+    "revision": "c80dbdeb0f3c33d6a038",
+    "url": "/recro/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87e7b4ec1d8fe154f728",
+    "url": "/recro/static/js/2.f6f599a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recro/static/js/2.f6f599a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c80dbdeb0f3c33d6a038",
+    "url": "/recro/static/js/main.54b069ce.chunk.js"
+  },
+  {
+    "revision": "c73a5f5e226e3e73efc9",
+    "url": "/recro/static/js/runtime-main.f58ce02f.js"
+  }
+]);
